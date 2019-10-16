012641f73204edd9a74ccf2242adfdb2bd378daf
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-16 14:45:36 -0700

    Second solution to LRU Cache

--- LRUCache.js
+++ LRUCache.js
@@ -73,3 +73,53 @@ class LRUCache {
   }
 }
 
+
+/////Solution 2//////
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+  this.cache = new Map();
+};
+
+/** 
+* @param {number} key
+* @return {number}
+*/
+LRUCache.prototype.get = function(key) {
+  //get(key) gets the value of key 
+  let temp = this.cache.get(key);
+  //if the value exists in cache 
+  //delete key from cache
+  //set the new key value pair in cache
+  //return temp
+  if (temp) {
+      this.cache.delete(key);
+      this.cache.set(key, temp);
+      return temp;
+  } else {
+      return -1; 
+  }
+};
+
+/** 
+* @param {number} key 
+* @param {number} value
+* @return {void}
+*/
+LRUCache.prototype.put = function(key, value) {
+  if (this.cache.has(key)) {
+      this.cache.delete(key);
+  } else if (this.cache.size >= this.capacity) {
+      this.cache.delete(this.cache.keys().next().value);
+  }
+  this.cache.set(key, value);
+};
+
+/** 
+* Your LRUCache object will be instantiated and called as such:
+* var obj = new LRUCache(capacity)
+* var param_1 = obj.get(key)
+* obj.put(key,value)
+*/
