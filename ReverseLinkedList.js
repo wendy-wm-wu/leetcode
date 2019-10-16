d3a1385c66c78c8b378e693b3b5432e69f0d991f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-16 10:52:08 -0700

    Reversed link list

---
+++ ReverseLinkedList.js
@@ -0,0 +1,21 @@
+/*
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+*/
+
+var reverseList = function(head) {
+  let curr = head;
+  let prev = null; 
+  
+  while (curr != null) {
+      let temp = curr.next; 
+      curr.next = prev; 
+      prev = curr; 
+      curr = temp; 
+  }
+  return prev; 
+}
