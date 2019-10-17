e57e9a9c1f9b7de6b18c83b357657507793616a2
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-17 10:50:01 -0700

    Solved using fibonacci

--- ClimbingStairs.js
+++ ClimbingStairs.js
@@ -21,3 +21,17 @@ Explanation: There are three ways to climb to the top.
 2. 1 step + 2 steps
 3. 2 steps + 1 step
 */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n < 0) {
+      return 0; 
+  } else if (n == 0) {
+      return 1; 
+  } else {
+      return climbStairs(n - 1) + climbStairs(n - 2);
+  }
+};
