b6be80788c175e0fe2bf3031cd3c88fe2aeee3c4
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-16 19:27:30 -0700

    Found 3 sum

--- 3Sum.js
+++ 3Sum.js
@@ -15,25 +15,50 @@ A solution set is:
   [-1, -1, 2]
 ]
 */
-
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var threeSum = function(nums) {
-  let curr = 0; 
-  let sum = 0; 
-  let output = [];
-  
-  if (nums.length <= 0) {
-      return [];
-  }
-  let helper = function(curr, arr) {
-      //base case 
-      if (curr[0] + curr[1] + curr[2] === 0 && curr.length === 3) {
-          output.push(curr);
-          return;
-      }
-      for (let i = 0; i < nums.length; i++) {
-          curr.push(nums[i]);
-      }
-  }
-  helper([], nums);
-  return output; 
-};
+  let results = [];
+     if (nums.length < 3) {
+         return results;
+     }
+     nums = nums.sort((a,b) => a - b); 
+     
+     let target = 0; 
+     
+     for (let i = 0; i < nums.length - 2; i++) {
+         if (nums[i] > target) {
+             break;
+         }
+         if (i > 0 && nums[i] === nums[i - 1]) {
+             continue; 
+         }
+         let j = i + 1; 
+         let k = nums.length - 1; 
+         
+         while (j < k) {
+             let sum = nums[i] + nums[j] + nums[k]; 
+             
+             if (sum === target) {
+                 results.push([nums[i], nums[j], nums[k]]);
+                 
+                 while (nums[j] === nums[j + 1]) {
+                     j++;
+                 }
+                 while (nums[k] === nums[k - 1]) {
+                     k--; 
+                 }
+                 j++;
+                 k--; 
+             } else if (sum < target) {
+                 j++;
+             } else if (sum > target) {
+                 k--;
+             }
+         }
+       
+     }
+     return results;
+ };
