13c1c7217ee26adc01eacac30c37e2c12e891912
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-17 16:44:25 -0700

    Found median of two sorted arrays

--- MedianOfTwoSortedArrays.js
+++ MedianOfTwoSortedArrays.js
@@ -18,3 +18,39 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  let totalLen = nums1.length + nums2.length;
+  let idx1 = 0;
+  let idx2 = 0;
+  let curr;
+  let prev; 
+  
+  while (idx1 + idx2 <= totalLen/2) {
+      if (curr !== undefined) {
+          prev = curr; 
+      }
+      let one = nums1[idx1];
+      let two = nums2[idx2];
+      
+      if (one === undefined) {
+          curr = two;
+          idx2++;
+      } else if (two === undefined) {
+          curr = one;
+          idx1++; 
+      } else if (one < two) {
+          curr = one;
+          idx1++;
+      } else {
+          curr = two;
+          idx2++;
+      }
+  }
+  return totalLen % 2 === 0 ? (prev + curr) / 2 : curr;
+};
