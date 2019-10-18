a1fc835ee06063e5c34d08fe3db2178034c5f7c8
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-17 22:36:09 -0700

    Merge intervals

--- MergeIntervals.js
+++ MergeIntervals.js
@@ -12,3 +12,29 @@ Input: [[1,4],[4,5]]
 Output: [[1,5]]
 Explanation: Intervals [1,4] and [4,5] are considered overlapping.
 */
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if (intverals.length === 0) {
+        return intervals; 
+    }
+    intervals.sort((a,b) => {
+        a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1];  
+    });
+    //define prev as the first in intervals 
+    let prev = intervals[0]; 
+    var res = [prev];
+    
+    for (var curr of intervals) {
+        if (curr[0] <= prev[1]) {
+            prev[1] = Math.max(prev[1], curr[1]); 
+        } else {
+            res.push(curr);
+            prev = curr; 
+        }
+    }
+    return res; 
+};
