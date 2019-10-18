629fbc6551ae727cc9c4b9b4129ca73cbda7465f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-17 22:47:33 -0700

    Merged intervals

--- MergeIntervals.js
+++ MergeIntervals.js
@@ -17,18 +17,28 @@ Explanation: Intervals [1,4] and [4,5] are considered overlapping.
  * @param {number[][]} intervals
  * @return {number[][]}
  */
-function merge(intervals) {
-  if (!intervals.length) return intervals
-  intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1])
-  var prev = intervals[0]
-  var res = [prev]
-  for (var curr of intervals) {
-    if (curr[0] <= prev[1]) {
-      prev[1] = Math.max(prev[1], curr[1])
-    } else {
-      res.push(curr)
-      prev = curr
-    }
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+  if (!intervals.length) {
+      return intervals;
   }
-  return res
-}
+  //sort intervals.. cannot assume it's sorted
+  intervals.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1]);
+  //define prev as the first item in intervals
+  let prev = intervals[0];
+  //define result as an array with the first array, prev
+  let result = [prev]; 
+  
+  for (let curr of intervals) {
+      if (curr[0] <= prev[1]) {
+          prev[1] = Math.max(prev[1], curr[1]); 
+      } else {
+          result.push(curr);
+          prev = curr; 
+      }
+  }
+  return result; 
+};
