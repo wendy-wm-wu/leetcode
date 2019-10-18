38fafcda0fbf1a62c0e921e50abac9bb2347be91
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-17 23:29:16 -0700

    Found happy number

--- HappyNumber.js
+++ HappyNumber.js
@@ -13,3 +13,26 @@ Explanation:
 62 + 82 = 100
 12 + 02 + 02 = 1
 */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  if (n < 1) {
+      return false;
+  }
+  let set = new Set(); 
+  
+  while (!set.has(n)) {
+      set.add(n);
+      let s = n.toString().split('');
+      n = 0;  
+      for (let i = 0; i < s.length; i++) {
+          n += (Number(s[i]) * Number(s[i]));
+      }
+      if (n === 1) {
+          return true; 
+      }
+  }
+  return false; 
+};
