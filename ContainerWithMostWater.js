a649b0c881019e44523f48ea8f59d04b031674be
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-26 15:42:02 -0700

    Solve container with most water problem

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -9,3 +9,29 @@ Input: [1,8,6,2,5,4,8,3,7]
 Output: 49
 */
 
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let max = 0; 
+  let left = 0;
+  let right = height.length - 1;
+  
+  while (right > left) {
+      let a = height[left];
+      let b = height[right];
+      
+      let area = Math.min(a,b) * (right - left);
+      
+      if (area > max) {
+          max = area;
+      }
+      if (b > a) {
+          left++;
+      } else {
+          right--;
+      }
+  }
+  return max;
+};
