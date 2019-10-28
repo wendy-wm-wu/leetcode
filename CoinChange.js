c054f1db2af5e9591340c380d7e42ba9596af622
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-28 16:02:11 -0700

    Find coin change

--- CoinChange.js
+++ CoinChange.js
@@ -11,3 +11,32 @@ Example 2:
 Input: coins = [2], amount = 3
 Output: -1
 */
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+  let res = Infinity;
+  
+  //we want to get the highest options first so we can sort 
+  coins = coins.sort((a,b) => b - a); 
+  
+  let helper = function(idx, amount, count) {
+      const coin = coins[idx];
+      
+      //smallest coin
+      if (idx === coins.length - 1) {
+          if (amount % coin === 0) {
+              res = Math.min(res, count + Math.floor(amount/coin))
+          }
+      } else {
+          for (let i = Math.floor(amount/coin); i >= 0 && count + i < res; i--) {
+              helper(idx + 1, amount - coin * i, count + i);
+          }
+      }
+  }
+  helper(0, count, 0); 
+  
+  return res === Infinity? -1 : res; 
+};
