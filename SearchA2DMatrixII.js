0db5efd2d6a90937e8076c02ca9928c0df60d7a7
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-10-28 15:59:13 -0700

    Search a 2d matrix

---
+++ SearchA2DMatrixII.js
@@ -0,0 +1,42 @@
+/*
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+Example:
+
+Consider the following matrix:
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+  if (matrix.length === 0 || matrix[0].length === 0) {
+      return false;
+  }
+  let i = 0; 
+  let j = matrix[0].length - 1;
+  
+  while (j >= 0 && i < matrix.length) {
+      if (matrix[i][j] === target) {
+          return true;
+      }
+      if (matrix[i][j] > target) {
+          j--;
+      } else if (matrix[i][j] < target) {
+          i++;
+      }
+  }
+  return false;
+};
