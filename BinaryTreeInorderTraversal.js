865c54f2df1d41a150676efa5a6a1949f49a16b3
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-04 17:24:47 -0800

    Complete binary tree inorder traversal

--- BinaryTreeInorderTraversal.js
+++ BinaryTreeInorderTraversal.js
@@ -13,3 +13,30 @@ Input: [1,null,2,3]
 Output: [1,3,2]
 Follow up: Recursive solution is trivial, could you do it iteratively?
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+  let res = [];
+  let stack = [];
+  
+  while (root || stack.length) {
+      if (root) {
+          stack.push(root);
+          root = root.left;
+      } else {
+          root = stack.pop();
+          res.push(root.val);
+          root = root.right;
+      }
+  }
+  return res;
+};
