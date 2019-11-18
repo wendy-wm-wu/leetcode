24920d834b53c0c08e595addec04a052129e055f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-18 11:03:55 -0800

    Find all anagrams in a string

--- FindAllAnagramsInAString.js
+++ FindAllAnagramsInAString.js
@@ -17,3 +17,31 @@ Explanation:
 The substring with start index = 0 is "cba", which is an anagram of "abc".
 The substring with start index = 6 is "bac", which is an anagram of "abc".
 */
+
+var findAnagrams = function(s, p) {
+    
+  const resultArray = [];
+  const pLength = p.length;
+  const sLength = s.length;
+
+  // create two empty arrays with 0 inside
+  const pWindow = new Array(26).fill(0);
+  const sWindow = new Array(26).fill(0);
+
+  //assume only a-z
+  //TODO: add checker later if input has invalid characters
+  [...p].forEach(character => {
+      // charCodeAt returns a--> 97, b --> 98, c--> 99, etc
+      pWindow[character.charCodeAt(0)-97]++
+  });
+  
+  [...s].forEach((character, index) => {
+      //jump into next position, and minus the previous chart from window
+      if (index >= pLength) sWindow[s.charCodeAt(index-pLength)-97]--
+      sWindow[character.charCodeAt(0)-97]++
+      // compare two strings
+      if (pWindow.join()===sWindow.join()) resultArray.push(index+1-pLength)
+  });
+
+  return resultArray
+};
