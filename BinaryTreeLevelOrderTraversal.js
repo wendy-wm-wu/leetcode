0300b224131b5a46acca0fff3aad6c330f948b44
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-21 11:18:08 -0800

    Solve binary tree level order traversal

--- BinaryTreeLevelOrderTraversal.js
+++ BinaryTreeLevelOrderTraversal.js
@@ -15,3 +15,59 @@ return its level order traversal as:
   [15,7]
 ]
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (!root) {
+      return [];
+  }
+  let output = [];
+  let queue = [];
+  queue.push(root);
+  
+  while (queue.length > 0) {
+      let size = queue.length;
+      let curr = [];
+      for (let i = 0; i < size; i++) {
+          let node = queue.shift();
+          curr.push(node.val);
+          if (node.left) {
+              queue.push(node.left);
+          }
+          if (node.right) {
+              queue.push(node.right);
+          }
+      }
+       output.push(curr);
+  }
+  return output; 
+};
+
+//define a queue
+//define output as []
+//push root into queue
+  //iterate thru root
+      //define node as shifted value from queue
+      //iterate thru size 
+          //define curr as [];
+          //if node has left
+              //push left node to queue
+          //if node has right
+              //push right node to queue
+          //push curr into output
+  //return output
+
+//input: tree
+//output: level order traversal of tree 
+//constraints: none
+//edge cases: if tree is null, return []
