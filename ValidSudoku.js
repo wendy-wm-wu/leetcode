cbd00508ccd300e4249d22aadb031979b62367fd
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-23 15:46:21 -0800

    Solve valid sudoku

---
+++ ValidSudoku.js
@@ -0,0 +1,64 @@
+/*
+var isValidSudoku = function(board) {
+    const rowRules = new Array(9).fill().map(() => new Set());
+    const colRules = new Array(9).fill().map(() => new Set());
+    const mixedRules = new Array(9).fill().map(() => new Set());
+    
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            let curr = board[row][col];
+            
+            const mixedIdx = Math.floor(row/3) * 3 + Math.floor(col/3);
+            
+            if (curr == '.') {
+                continue;
+            }
+            
+            const a = rowRules[row].has(curr)
+            let b = colRules[col].has(curr);
+            let c = mixedRules[mixedIdx].has(curr);
+            
+            if (a || b || c) {
+                return false;
+            }
+            
+            rowRules[row].add(curr);
+            colRules[col].add(curr);
+            mixedRules[mixedIdx].add(curr);
+            
+        }
+    }
+    return true;
+};
+*/
+var isValidSudoku = function(board) {
+  const rowRules = new Array(9).fill().map(() => new Set());
+  const colRules = new Array(9).fill().map(() => new Set());
+  const mixedRules = new Array(9).fill().map(() => new Set());
+  
+  for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+          let curr = board[row][col];
+          
+          const mixedIdx = Math.floor(row/3) * 3 + Math.floor(col/3);
+          
+          if (curr == '.') {
+              continue;
+          }
+          
+          const a = rowRules[row].has(curr)
+          let b = colRules[col].has(curr);
+          let c = mixedRules[mixedIdx].has(curr);
+          
+          if (a || b || c) {
+              return false;
+          }
+          
+          rowRules[row].add(curr);
+          colRules[col].add(curr);
+          mixedRules[mixedIdx].add(curr);
+          
+      }
+  }
+  return true;
+};
