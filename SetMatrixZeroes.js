2be3cdde8767c4407c1e99810f0c40baf1871b73
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-25 13:17:40 -0800

    Set zeroes in matrix

--- SetMatrixZeroes.js
+++ SetMatrixZeroes.js
@@ -30,3 +30,55 @@ Output:
   [0,3,1,0]
 ]
 */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+  let firstColHasZeros = false;
+  let firstRowHasZeros = false;
+    //check if first col has 0 
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === 0) {
+            firstColHasZeros = true;
+            break;
+        } 
+    }
+   //check if first row has 0
+    for (let j = 0; j < matrix[0].length; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowHasZeros = true;
+            break;
+        }
+    }
+        
+    //iterate thru matrix and use first row and first col as flags
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0; 
+            }
+        }
+    }
+    //zero out cells based on flags in first row and col
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 1; j < matrix[0].length; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    if (firstColHasZeros) {
+         for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+    if (firstRowHasZeros) {
+        for (let j = 0; j < matrix[0].length; j++) {
+            matrix[0][j] = 0; 
+        }
+    }
+    
+};
