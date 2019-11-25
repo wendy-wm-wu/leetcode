743a0c454cbfbf6909daa9aba50f1ca1664d2cbe
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-25 13:15:16 -0800

    Find unique paths using DP

--- UniquePaths.js
+++ UniquePaths.js
@@ -6,38 +6,28 @@ The robot can only move either down or right at any point in time. The robot is 
 How many possible unique paths are there?
 */var uniquePaths = function(m, n) {
 
- // Init board.
- let board = Array(m).fill(null).map(()=>{
-  return Array(n).fill(0)
-})
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  var a = new Array(n).fill(1);
+  console.log(a);
+  var dp = new Array(m).fill(a);
+  console.log(dp);
+  for (var i = 1; i < m; i++) {
+      for (var j = 1; j < n; j++) {
+          dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+  }
+  return dp[m - 1][n - 1];
+};
 
-// Base case.
-// There is only one way of getting to board[0][any] and
-// there is also only one way of getting to board[any][0].
-// This is because we have a restriction of moving right
-// and down only.
+/*
 
-for (let rowIndex = 0; rowIndex < m; rowIndex += 1) {
-for (let columnIndex = 0; columnIndex < n; columnIndex += 1) {
- if (rowIndex === 0 || columnIndex === 0) {
-   board[rowIndex][columnIndex] = 1;
- }
-}
-}
 
-// Now, since we have this restriction of moving only to the right
-// and down we might say that number of unique paths to the current
-// cell is a sum of numbers of unique paths to the cell above the
-// current one and to the cell to the left of current one.
 
-for (let rowIndex = 1; rowIndex < m; rowIndex += 1) {
-for (let columnIndex = 1; columnIndex < n; columnIndex += 1) {
- const uniquesFromTop = board[rowIndex - 1][columnIndex];
- const uniquesFromLeft = board[rowIndex][columnIndex - 1];
- board[rowIndex][columnIndex] = uniquesFromTop + uniquesFromLeft;
-}
-}
-
-return board[m - 1][n - 1];
+*/
 
 };
