e863165c33fee9f76d27f017dda979253bedbab1
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-25 21:34:55 -0800

    Lowest common ancestor

--- LowestCommonAncestor.js
+++ LowestCommonAncestor.js
@@ -33,22 +33,13 @@ var lowestCommonAncestor = function(root, p, q) {
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
-  if (!root || root === p || root === q) {
-      return root; 
-  }
-  const left = lowestCommonAncestor(root.left, p, q);
-  console.log(left);
-  const right = lowestCommonAncestor(root.right, p, q);
-  console.log(right);
-  
-  if (!left) {
-      return right;
-  }
-  if (!right) {
-      return left; 
-  }
-  return root;
+const lowestCommonAncestor = (root, p, q) => {
+  if (!root || root === p || root === q) return root
+  const left = lowestCommonAncestor(root.left, p, q)
+  const right = lowestCommonAncestor(root.right, p, q)
+  if (!left) return right  // p and q are in the right subtree
+  if (!right) return left  // p and q are in the left subtree
+  return root              // p is in one side and q is in the other
 };
 
 /*
