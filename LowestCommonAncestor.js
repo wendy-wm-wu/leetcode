fc118fd25ac311801430d12b662759d49337a49d
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-11-25 21:19:54 -0800

    Find lowest common ancestor

--- LowestCommonAncestor.js
+++ LowestCommonAncestor.js
@@ -20,5 +20,53 @@ Given the following binary tree:  root = [3,5,1,6,2,0,8,null,null,7,4]
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    
+    /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (!root || root === p || root === q) {
+      return root; 
+  }
+  const left = lowestCommonAncestor(root.left, p, q);
+  console.log(left);
+  const right = lowestCommonAncestor(root.right, p, q);
+  console.log(right);
+  
+  if (!left) {
+      return right;
+  }
+  if (!right) {
+      return left; 
+  }
+  return root;
+};
+
+/*
+const left =  lowestCommonAncestor(root.left, p, q)
+const right =  lowestCommonAncestor(root.right, p, q)
+
+if (!left) {
+  return right; 
+}
+if (!right) {
+  return left;
+}
+return root; 
+
+*/
+
+//input: tree, p node, q node
+//output: lowest common ancestor
+//constraints: none
+//edge cases: if root is null or root = p or root = q, return root 
 };
