79c383f3ea3058633aabf8e01c37da707f9b9994
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-12-13 14:59:01 -0800

    Check get permutations problem

--- getPermutations.js
+++ getPermutations.js
@@ -4,7 +4,20 @@ let string = 'cat';
 //output: cat, cta, atc, act, tac, tca 
 
 function getPermutations(string) {
-  
+  let permutations = new Set(); 
+  let copy = string.slice();
+
+  let helper = function(curr, stringRemaining) {
+    if (curr.length === string.length) {
+      permutations.add(curr); 
+    }
+    for (let i = 0; i < copy.length; i++) {
+      let letter = copy[i];
+      helper(curr + letter, copy.slice(0, i) + copy.slice(i + 1);
+    }
+  }
+  helper('', copy);
+  return permutations;
 }
 
 console.log(getPermutations(string)); 
