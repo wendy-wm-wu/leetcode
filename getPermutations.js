e0e4bf3be5fd6df591df2243a3e89568cd6b38b4
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-12-13 14:53:12 -0800

    Set up get permutations problem

---
+++ getPermutations.js
@@ -0,0 +1,10 @@
+// Write a recursive function for generating all permutations of an input string. Return them as a set. 
+
+let string = 'cat';
+//output: cat, cta, atc, act, tac, tca 
+
+function getPermutations(string) {
+  
+}
+
+console.log(getPermutations(string)); 
