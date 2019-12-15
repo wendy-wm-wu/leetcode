fa968889618d1c96cc5354ef03acafa438fef526
wendy-wm-wu <wendy.wm.wu@gmail.com>
2019-12-15 12:20:09 -0800

    Set up find duplicate number problem

---
+++ findDuplicateNumber.js
@@ -0,0 +1,9 @@
+let array = [1, 2, 3, 4, 5, 6, 6, 7, 8, 9, 10];
+let n = 10; 
+
+function findDuplicateNumber(array, n) {
+  let sum = ((n * n) + n)/2; 
+  console.logs(sum); 
+}
+
+console.log(findDuplicateNumber(array, n)); 
