08f937332518e1d74408b93c1142b78e20be6f8f
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-01-05 14:59:48 -0800

    Prove valid anagram

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -15,3 +15,28 @@ You may assume the string contains only lowercase alphabets.
 Follow up:
 What if the inputs contain unicode characters? How would you adapt your solution to such case?
 */
+
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false; 
+  let counts = {};
+  for (let i = 0; i < s.length; i++) {
+    let item = s[i];
+    if (!counts[item]) {
+      counts[item] = 1; 
+    } else {
+      counts[item]++; 
+    }
+  }
+  for (let j = 0; j < t.length; j++) {
+    let letter = t[j]; 
+    if (!counts[letter]) {
+      return false; 
+    } else {
+      counts[letter]--; 
+    }
+  }
+  return true; 
+}
+
+let s = "anagram", t = "nagaram"; 
+console.log(isAnagram(s, t)); 
