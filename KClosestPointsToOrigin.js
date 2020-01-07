153576d04a3ccfd104f3070e11d75dcf97b0ea2c
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-01-07 15:35:02 -0800

    Find k closest points to origin

--- KClosestPointsToOrigin.js
+++ KClosestPointsToOrigin.js
@@ -1,13 +1,8 @@
 /*
 
 We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
-
-(Here, the distance between two points on a plane is the Euclidean distance.)
-
+(Here, the distance between two points on a plane is the Euclidean distance.)=
 You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
-
- 
-
 Example 1:
 
 Input: points = [[1,3],[-2,2]], K = 1
@@ -23,16 +18,28 @@ Input: points = [[3,3],[5,-1],[-2,4]], K = 2
 Output: [[3,3],[-2,4]]
 (The answer [[-2,4],[3,3]] would also be accepted.)
 */
+let points = [[3,3],[5,-1],[-2,4]];
+let K = 2; 
 
-/**
- * @param {number[][]} points
- * @param {number} K
- * @return {number[][]}
- */
 var kClosest = function(points, K) {
-  return points.sort((a,b) => distance(a) - distance(b)).slice(0, K);
+  return points.sort((a,b) => distance(a) - distance(b)).slice(0, K); 
 };
 
 var distance = function(a) {
-  return a[0] * a[0] + a[1] * a[1];
+  return a[0] * a[0] + a[1] * a[1]; 
 }
+
+console.log(kClosest(points, K)); 
+
+/*
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+3^2 + 3^2 = 18
+5^2 + -1(^2) = 26
+-2(^2) + 4^2 = 4 + 16 = 20 
+
+distance formula used inside sorting function
+multiply a[0] * a[0] + a[1] * a[1]
+
+sort possibilities and slice by k options 
+*/
