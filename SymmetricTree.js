7c331f2c80a6ab2dfebd45a6e09ff6f55df5b6ae
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-02-18 20:27:35 -0800

    Add iterative approach to finding if tree is symmetric

--- SymmetricTree.js
+++ SymmetricTree.js
@@ -34,6 +34,11 @@ Bonus points if you could solve it both recursively and iteratively.
  * @param {TreeNode} root
  * @return {boolean}
  */
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
 var isSymmetric = function(root) {
   if (!root) return true; 
   function isMirror(s, t) {
@@ -44,6 +49,25 @@ var isSymmetric = function(root) {
   return isMirror(root.left, root.right); 
 };
 
-/*
-compare both subtrees to see if they're a mirror of each other 
-*/
+
+/*compare both subtrees to see if they're a mirror of each other */
+
+/* Iterative Solution */
+
+function isMirror(p, q) {
+  //create 2 stacks
+  let s1 = [p], s2 = [q]; 
+  //perform preorder traversal
+  while(s1.length > 0 || s2.length > 0) {
+    var n1 = s1.pop(), n2 = s2.pop(); 
+    if (!n1 && !n2) continue; 
+    if (!n1 || !n2 || n1.val !== n2.val) return false; //if mismatch
+    s1.push(n1.left); 
+    s1.push(n1.right);
+    s2.push(n2.left);
+    s2.push(n2.right); 
+  }
+  return true; 
+}
+
+
