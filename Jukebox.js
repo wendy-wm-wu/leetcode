7dde1d3781dcd3e775d91f30e78d625ed563cae3
wendy-wm-wu <wendy.wm.wu@gmail.com>
2020-03-01 16:39:23 -0800

    Remove all values from array

--- Jukebox.js
+++ Jukebox.js
@@ -1,42 +1,42 @@
 var Jukebox = function() {
-  this.currentSong = null;
-  this.songQueue = [];
+  this.currentSong = null; //current song playing
+  this.songQueue = []; //queue of all the songs to be played 
 }
 
-Jukebox.prototype.shuffle = function() {
-  this.songQueue.sort(() => Math.random() > 0.5 ? 1 : 0); 
-}
-
-Jukebox.prototype.play = function(song) {
+Jukebox.prototype.play = function() {
   if (this.currentSong === null) {
-    this.currentSong = this.songQueue.unshift();
-    this.currentSong.play(); 
+    this.currentSong = this.songQueue.unshift(); 
+    this.currentSong.play();
   } else {
-    console.log('song is currently playing'); 
+    console.log('song is playing')
   }
+}
+
+Jukebox.prototype.nextSong = function() {
+  this.currentSong = this.songQueue.unshift();
+  this.currentSong.play();
 }
 
 Jukebox.prototype.pause = function() {
   if (this.currentSong !== null) {
     this.currentSong.pause(); 
   } else {
-    console.log('there is no song currently playing');
+    console.log('no song is playing')
   }
 }
 
-Jukebox.prototype.nextSong = function() {
-  this.songQueue.shift();
-  this.currentSong = this.songQueue[0]; 
-  this.currentSong.play(); 
-}
-
-Jukebox.prototype.addSong = function(song) {
-  this.songQueue.push(song);
+Jukebox.prototype.shuffle = function() {
+  return this.songQueue.sort(() => Math.random() > 0.5 ? 1: 0); 
 }
 
 Jukebox.prototype.currentSongName = function() {
-  return this.currentSong.name;
+  return this.currentSong.name; 
 }
+
+Jukebox.prototype.addSong = function(song) {
+  this.songQueue.push(song); 
+}
+
 
 var Song = function(name, track) {
   this.name = name;
@@ -44,10 +44,9 @@ var Song = function(name, track) {
 }
 
 Song.prototype.play = function() {
-  this.track.play(); 
+  this.track.play();
 }
 
 Song.prototype.pause = function() {
   this.track.pause();
 }
-
